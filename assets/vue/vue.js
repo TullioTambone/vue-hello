cc879714ac5fd0f982c2815f7fec642421c5863d
TullioTambone <tulliotambone97@gmail.com>
2023-04-18 13:08:47 +0200

    fine esercitazione

--- assets/vue/vue.js
+++ assets/vue/vue.js
@@ -1,9 +1,36 @@
-const {createApp} = vue;
+const {createApp} = Vue;
 
 createApp({
   data() {
     return {
-
+      message: "Hello World!",
+      image: "https://picsum.photos/id/237/200/300",
+      colorBlack: "color-black",
+      message2: "clicca per cambiare immagine",
     }
-  }
+  },
+  methods: {
+    changeColor(){
+        if (this.colorBlack === "color-black") {
+            this.colorBlack = "color-red";
+        } else{
+            this.colorBlack = "color-black";
+        }
+    },
+
+   
+
+    changeImage(){
+
+        this.image = `https://picsum.photos/id/${myRandom()}/200/300`;
+
+        function myRandom (){
+            return Math.floor(Math.random() * 300) + 200;
+        }
+    },
+
+   
+  },
 }).mount('#app')
+
+
